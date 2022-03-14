76e91f0c761ee19de437172e8c38012a2fc0e963
Mikael Ståldal <mikael.staldal@chromaway.com>
2022-03-14 16:28:49 +0100

    Maven 3.8.5

--- maven/setup.sh
+++ maven/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build --build-arg JAVA_VERSION=11 --build-arg MAVEN_VERSION=3.8.4 --tag maven:3.8.4-Java11 .
+docker build --build-arg JAVA_VERSION=11 --build-arg MAVEN_VERSION=3.8.5 --tag maven:3.8.5-Java11 .
 
 mkdir -p ~/.m2
