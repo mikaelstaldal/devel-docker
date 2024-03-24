be9455de7ed0066781801ddbd8fad729adbbd229
Mikael Ståldal <mikael@staldal.nu>
2024-03-24 18:14:25 +0100

    Use Java 17

--- maven/setup.sh
+++ maven/setup.sh
@@ -2,4 +2,4 @@
 
 docker build --build-arg JAVA_VERSION=17 --build-arg MAVEN_VERSION=3.9.3 --tag maven:3.9.3-Java17 .
 
-mkdir -p ~/.m2
+mkdir -p ~/.m2 ~/.java
--- sbt/setup.sh
+++ sbt/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker build --build-arg JAVA_VERSION=11 --build-arg SCALA_VERSION=2.13.7 --build-arg SBT_VERSION=1.5.8 --tag sbt:1.5.8-Scala2.13.7-Java11 .
-docker build --build-arg JAVA_VERSION=11 --build-arg SCALA_VERSION=3.1.0 --build-arg SBT_VERSION=1.5.8 --tag sbt:1.5.8-Scala3.1.0-Java11 .
+docker build --build-arg JAVA_VERSION=17 --build-arg SCALA_VERSION=2.13.7 --build-arg SBT_VERSION=1.5.8 --tag sbt:1.5.8-Scala2.13.7-Java17 .
+docker build --build-arg JAVA_VERSION=17 --build-arg SCALA_VERSION=3.1.0 --build-arg SBT_VERSION=1.5.8 --tag sbt:1.5.8-Scala3.1.0-Java17 .
 
 mkdir -p ~/.sbt
 mkdir -p ~/.ivy2
