8f9b7ed37d60baf0f270a1d4867317ee6ea6ef57
Mikael Ståldal <mikael@staldal.nu>
2024-03-24 17:07:27 +0100

    Use Java 17 and Maven 3.9.3

--- maven/setup.sh
+++ maven/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build --build-arg JAVA_VERSION=11 --build-arg MAVEN_VERSION=3.8.5 --tag maven:3.8.5-Java11 .
+docker build --build-arg JAVA_VERSION=17 --build-arg MAVEN_VERSION=3.9.3 --tag maven:3.9.3-Java17 .
 
 mkdir -p ~/.m2
